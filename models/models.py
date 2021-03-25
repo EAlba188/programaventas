0bc1c5b323d4c42b2b54b3ece48d2c5309050132
EAlba188 <edenalba188@gmail.com>
2021-03-25 10:42:52 +0100

    2

--- models/models.py
+++ models/models.py
@@ -7,8 +7,8 @@ class Producto(models.Model):
     _name = 'programaventas.producto'
 
     id = fields.Char(required=True, size=20, string="ID")
-    nombre = fields.Char(string="nombre")
-    precio = fields.Float(string="precio", digits=(6,2))
+    name = fields.Char(string="Nombre Producto")
+    precio = fields.Float(string="Precio", digits=(6,2))
     numeroVentas = fields.Integer(readonly=1, string="Numero Ventas")
     brutoGenerado = fields.Float(string="Bruto Total", readonly=1, default=0)
 
@@ -21,28 +21,24 @@ class Producto(models.Model):
         self.numeroVentas=res
 
 
-class Venta(models.Model):                  #añadir forma de pago
+class Venta(models.Model):
     _name = 'programaventas.venta'
 
+
     #CREAR NUMERO DE REFERENCIA AUTOINCREMENTAL
-    precio = fields.Float(string="Precio", digits=(6,2), readonly=1)
     dni = fields.Many2one('programaventas.cliente', string="Cliente")
     idproducto = fields.Many2one('programaventas.producto', string="Producto")
+    precio = fields.Float(string="Precio", digits=(6,2), readonly=1, related='idproducto.precio')
     fecha = fields.Date(string="Fecha", default=datetime.today(), readonly=1)
-    pago = fields.Float(string="Precio pagado")
+    pago = fields.Selection(string="Forma de pago", selection=[('efectivo',"Efectivo"),('tarjeta',"Tarjeta")])
 
-    @api.onchange('idproducto')
-    def setPrecio(self):
-        for producto in Producto:
-            if self.idproducto == producto.id:
-                self.precio=producto.precio
 
 
 class Cliente(models.Model):
     _name = 'programaventas.cliente'
 
-    dni = fields.Char(required=True, string="DNI Cliente")
-    nombre = fields.Char(string="Nombre")
+    dni = fields.Char(required=True, string="DNI Cliente", size=9)
+    name = fields.Char(string="Nombre")
     numeroCompras = fields.Integer(readonly=1, string="Numero de compras", default=0)
     brutoGenerado = fields.Float(string="Bruto Total", readonly=1, default=0)
 
